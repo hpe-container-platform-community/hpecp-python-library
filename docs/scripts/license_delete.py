800b91de624051e05ed7f361d572a9b6a1740617
chris snow <chsnow123@gmail.com>
2020-04-27 21:31:48 +0100

    refactor example scripts

--- docs/scripts/license_delete.py
+++ docs/scripts/license_delete.py
@@ -12,11 +12,11 @@ client = ContainerPlatformClient(username='admin',
 
 client.create_session()
 
-try:
-    print( client.license.register_license("/srv/bluedata/license/LICENSE") )
-    time.sleep(5)
-except:
-    pass # Ignore errors if license already registered
+license = client.license.get_license()
+print( license )
+
+print( client.license.delete_license( license['Licenses'][0]['LicenseKey']) )
+time.sleep(5)
 
 license = client.license.get_license()
 print( license )
