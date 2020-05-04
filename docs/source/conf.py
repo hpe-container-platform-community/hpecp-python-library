0457eb11f7f5e29bfa0f7b87dabcde4f9554da11
chris snow <chsnow123@gmail.com>
2020-05-04 17:16:27 +0100

    add links to source code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,8 @@ author = 'Chris Snow'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
