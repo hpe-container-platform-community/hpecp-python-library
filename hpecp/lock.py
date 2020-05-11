d40c8a9b11ead6bf72ea5a39c52ded1f5de0afe5
chris snow <chsnow123@gmail.com>
2020-05-11 20:44:31 +0100

    add locks to cli

--- hpecp/lock.py
+++ hpecp/lock.py
@@ -8,36 +8,33 @@ import requests
 import json
 import urllib
 import polling
+import re
 
 class LockController:
 
     def __init__(self, client):
         self.client = client
 
-    """
-    Example response:
+    def get(self):
+        return self.client._request(url='/api/v1/lock', http_method='get', description='lock/get_locks').json()
 
-    {'_links': {'self': {'href': '/api/v1/lock'}}, 'locked': True, 'quiesced': True, '_embedded': {'internal_locks': [], 'external_locks': [{'_links': {'self': {'href': '/api/v1/lock/2'}}, 'reason': 'install', 'internal': False, 'source': 'admin'}]}}
-    """
-    def get_locks(self):
-        response = self.client._request(url='/api/v1/lock', http_method='get', description='lock/get_locks')
-        return response.json()
-
-    def set_lock(self, reason=None):
+    def create(self, reason=None):
         data = {"reason" : reason}
-        response = self.client._request(url='/api/v1/lock', http_method='post', data=data, description='lock/set_lock')
-        return response
+        return self.client._request(url='/api/v1/lock', http_method='post', data=data, description='lock/set_lock')
 
-    def delete_lock(self, lock_id):
-        response = self.client._request(url='/api/v1/lock/{}'.format(lock_id), http_method='delete', description='lock/delete_lock')
-        return response
+    def delete(self, lock_id):
 
-    def delete_locks(self, timeout_secs=300):
+        assert isinstance(lock_id, str),"'lock_id' must be provided and must be a string"
+        assert re.match(r'\/api\/v1\/lock\/[0-9]+', lock_id), "'lock_id' must have format '/api/v1/lock/[0-9]+'"
+
+        return self.client._request(url=lock_id, http_method='delete', description='lock/delete_lock')
+
+    def delete_all(self, timeout_secs=300):
         # we can only wait for internal locks - these need to be cleared
         # before external locks
         try:
             polling.poll(
-                lambda: len(self.get_locks()['_embedded']['internal_locks']) == 0,
+                lambda: len(self.get()['_embedded']['internal_locks']) == 0,
                 step=60,
                 poll_forever=False,
                 timeout=timeout_secs
@@ -45,8 +42,10 @@ class LockController:
         except polling.TimeoutException:
             raise Exception("Timed out waiting for internal locks to free.")
 
-        if len(self.get_locks()['_embedded']['external_locks']) > 0:
-            for lock in self.get_locks()['_embedded']['external_locks']:
-                lock_id = lock['_links']['self']['href'].split('/')[-1]
-                self.delete_lock(lock_id)
+        if len(self.get()['_embedded']['external_locks']) > 0:
+            for lock in self.get()['_embedded']['external_locks']:
+                lock_id = lock['_links']['self']['href']
+                self.delete(lock_id)
+
+        return True
 
