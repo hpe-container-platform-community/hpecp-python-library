bd6fea5525c57cb6e62b6a8fd393964de0ef319a
chris snow <chsnow123@gmail.com>
2020-05-13 02:55:24 +0100

    add locks doc

--- hpecp/lock.py
+++ hpecp/lock.py
@@ -21,13 +21,40 @@ class LockController:
         self.client = client
 
     def get(self):
+        """Retrieve the locks"""
         return self.client._request(url='/api/v1/lock', http_method='get', description='lock/get_locks').json()
 
+    def list(self):
+        """Retrieve the locks"""
+        return self.get()
+
     def create(self, reason=None):
+        """Create a new lock
+
+        Arguments:
+
+            reason: str
+                Provide a reason for the lock
+
+        Raises:
+
+            APIException
+        """
         data = {"reason" : reason}
         return self.client._request(url='/api/v1/lock', http_method='post', data=data, description='lock/set_lock')
 
     def delete(self, lock_id):
+        """Delete a lock
+
+        Arguments:
+
+            lock_id: str
+                The lock id as retrieved with `get()`  Format: '/api/v1/lock/[0-9]+'
+
+        Raises:
+
+            APIException
+        """
 
         assert isinstance(lock_id, basestring),"'lock_id' must be provided and must be a string"
         assert re.match(r'\/api\/v1\/lock\/[0-9]+', lock_id), "'lock_id' must have format '/api/v1/lock/[0-9]+'"
@@ -35,8 +62,18 @@ class LockController:
         return self.client._request(url=lock_id, http_method='delete', description='lock/delete_lock')
 
     def delete_all(self, timeout_secs=300):
-        # we can only wait for internal locks - these need to be cleared
-        # before external locks
+        """Delete all locks
+
+        Arguments:
+
+            timeout_secs: int
+                How long to wait for internal locks (note these need to be cleared before external locks can be deleted)
+
+        Raises:
+
+            APIException
+        """
+
         try:
             polling.poll(
                 lambda: len(self.get()['_embedded']['internal_locks']) == 0,
