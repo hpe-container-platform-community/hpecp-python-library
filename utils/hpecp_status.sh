dfd0d1e06cf7738a923c72c876ab84f3353b73e3
Chris Snow <chsnow123@gmail.com>
2020-05-25 21:19:46 +0000

    add aws scripts

---
+++ utils/hpecp_status.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ ! -d ~/.aws ]]; then
+   echo "Please run 'aws configure'"
+   exit 1
+fi
+
+if [[ ! -f ~/.aws_instance_ids ]]; then
+    echo "Please input a list of instance ids, separated by whitespace:"
+    read INSTANCE_IDS
+    echo "Saving instance ids to ~/.aws_instance_ids"
+    echo $INSTANCE_IDS > ~/.aws_instance_ids
+fi
+
+INSTANCE_IDS=$(cat ~/.aws_instance_ids)
+
+aws ec2 describe-instance-status --instance-ids  $INSTANCE_IDS --include-all-instances --output table --query "InstanceStatuses[*].{ID:InstanceId,State:InstanceState.Name}"
