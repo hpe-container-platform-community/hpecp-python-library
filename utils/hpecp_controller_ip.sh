54ed42dfeafc9ca3ff0277b8cd712e98a8047c07
Chris Snow <chsnow123@gmail.com>
2020-05-27 04:42:36 +0000

    fix util scripts

--- utils/hpecp_controller_ip.sh
+++ utils/hpecp_controller_ip.sh
@@ -3,10 +3,24 @@
 set -u
 set -e
 
-if [[ ! -d ~/.aws || ! -f ~/.hpecp_service ]]; then
-   echo "Please run utils/hpecp_configure.sh"
-   exit 1
+if [[ ! -d ~/.aws  ]]; then
+   if [[ ! -d /workspace/.aws ]]; then
+       echo "Please run utils/hpecp_configure.sh"
+       exit 1
+   else
+       ln -sf /workspace/.aws ~/.aws
+   fi
 fi
+
+if [[ ! -f ~/.hpecp_service  ]]; then
+   if [[ ! -f /workspace/.hpecp_service ]]; then
+       echo "Please run utils/hpecp_configure.sh"
+       exit 1
+   else
+       ln -sf /workspace/.hpecp_service ~/.hpecp_service
+   fi
+fi
+
 source ~/.hpecp_service
 
 aws ec2 describe-instances --instance-ids $CONTROLLER_INSTANCE_ID --output text --query "Reservations[*].Instances[*].[PublicIpAddress]"
--- utils/hpecp_firewall.sh
+++ utils/hpecp_firewall.sh
@@ -2,9 +2,22 @@
 
 set -u
 
-if [[ ! -d ~/.aws || ! -f ~/.hpecp_service ]]; then
-   echo "Please run utils/hpecp_configure.sh"
-   exit 1
+if [[ ! -d ~/.aws  ]]; then
+   if [[ ! -d /workspace/.aws ]]; then
+       echo "Please run utils/hpecp_configure.sh"
+       exit 1
+   else
+       ln -sf /workspace/.aws ~/.aws
+   fi
+fi
+
+if [[ ! -f ~/.hpecp_service  ]]; then
+   if [[ ! -f /workspace/.hpecp_service ]]; then
+       echo "Please run utils/hpecp_configure.sh"
+       exit 1
+   else
+       ln -sf /workspace/.hpecp_service ~/.hpecp_service
+   fi
 fi
 source ~/.hpecp_service
 
