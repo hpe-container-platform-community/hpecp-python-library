724def7ae44ccf3a1c387e6e013846373f3f6bcf
Chris Snow <chsnow123@gmail.com>
2020-06-04 08:59:16 +0000

    remove output from set_storage

--- bin/cli.py
+++ bin/cli.py
@@ -351,12 +351,10 @@ class K8sWorkerProxy(object):
         p_disks = persistent_disks.split(",")
         e_disks = ephemeral_disks.split(",")
 
-        print(
-            get_client().k8s_worker.set_storage(
-                worker_id=k8sworker_id,
-                persistent_disks=p_disks,
-                ephemeral_disks=e_disks,
-            )
+        get_client().k8s_worker.set_storage(
+            worker_id=k8sworker_id,
+            persistent_disks=p_disks,
+            ephemeral_disks=e_disks,
         )
 
     def wait_for_status(
