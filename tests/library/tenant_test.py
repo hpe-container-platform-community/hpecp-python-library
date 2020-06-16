68ace81e616ae82d96221faf66c067038a48d5a3
Chris Snow <chsnow123@gmail.com>
2020-06-16 13:07:13 +0000

    fix flake issue
    Signed-off-by: Chris Snow <chsnow123@gmail.com>

--- tests/library/tenant_test.py
+++ tests/library/tenant_test.py
@@ -18,7 +18,7 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-from unittest import TestCase, skip
+from unittest import TestCase
 
 import requests
 from mock import patch
