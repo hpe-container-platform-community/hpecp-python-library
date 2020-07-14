97466df097359b51eda06d6facb6bf0bc78e805b
Chris Snow <chsnow123@gmail.com>
2020-07-14 20:01:59 +0000

    add lock cli test
    Signed-off-by: Chris Snow <chsnow123@gmail.com>

---
+++ tests/library/lock_test.py
@@ -0,0 +1,84 @@
+# (C) Copyright [2020] Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+
+import os
+import sys
+import unittest
+from textwrap import dedent
+import json
+import yaml
+
+import requests
+from mock import patch
+
+from hpecp import ContainerPlatformClient
+from hpecp.exceptions import APIItemNotFoundException
+import tempfile
+from hpecp.base_resource import ResourceList
+
+from .base_test import BaseTestCase, MockResponse
+import six
+
+
+class TestCLIList(BaseTestCase):
+    def mocked_requests_get(*args, **kwargs):
+        if args[0] == "https://127.0.0.1:8080/api/v1/lock":
+            return MockResponse(
+                json_data={
+                    "_links": {"self": {"href": "/api/v1/lock"}},
+                    "locked": False,
+                    "_embedded": {"internal_locks": [], "external_locks": []},
+                },
+                status_code=200,
+                headers=dict(),
+            )
+        raise RuntimeError("Unhandle GET request: " + args[0])
+
+    @patch("requests.post", side_effect=BaseTestCase.mocked_requests_post)
+    @patch("requests.get", side_effect=mocked_requests_get)
+    def test_list(self, mock_post, mock_get):
+
+        try:
+            hpecp = self.cli.CLI()
+            hpecp.lock.list()
+        except Exception as e:
+            # Unexpected Exception
+            self.fail(e)
+
+        stdout = self.out.getvalue().strip()
+        stderr = self.err.getvalue().strip()
+
+        expected_stdout = """\
+_embedded:
+  external_locks: []
+  internal_locks: []
+_links:
+  self:
+    href: /api/v1/lock
+locked: false"""
+
+        expected_stderr = ""
+
+        self.assertEqual(stdout, expected_stdout)
+
+        # coverage seems to populate standard error on PY3 (issues 93)
+        if six.PY2:
+            self.assertEqual(stderr, expected_stderr)
