e641bc4f611bd6d768ad361ab4cb157c4cf4a541
Chris Snow <chsnow123@gmail.com>
2020-07-28 08:30:16 +0000

    fix(build): py 35 and 27
    Signed-off-by: Chris Snow <chsnow123@gmail.com>

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def get_version(rel_path):
         raise RuntimeError("Unable to find version string.")
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 
